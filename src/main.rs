2e13a8d386e2cebdffa4dfc9c43497ed2355c975
Khue Nguyen <Z5483Y@gmail.com>
2021-04-18 21:58:07 -0400

    feat: initialize terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::{io, panic};
+
+use crossterm::{cursor, execute, terminal};
+
+fn setup_terminal() {
+    let mut stdout = io::stdout();
+
+    execute!(stdout, cursor::Hide).unwrap();
+    execute!(stdout, terminal::EnterAlternateScreen).unwrap();
+
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+
+    terminal::enable_raw_mode().unwrap();
+}
+
+fn cleanup_terminal() {
+    let mut stdout = io::stdout();
+
+    execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
+    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+
+    execute!(stdout, terminal::LeaveAlternateScreen).unwrap();
+    execute!(stdout, cursor::Show).unwrap();
+
+    terminal::disable_raw_mode().unwrap();
+}
+
+fn setup_panic_hook() {
+    panic::set_hook(Box::new(|panic_info| {
+        cleanup_terminal();
+        better_panic::Settings::auto().create_panic_handler()(panic_info);
+    }));
+}
+
 fn main() {
-    println!("Hello, world!");
+    better_panic::install();
+
+    setup_panic_hook();
+    setup_terminal();
 }
